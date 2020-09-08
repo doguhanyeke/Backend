8083f377b81ae139b0bbb0e49388ffad63290ed6
doguhanyeke <doguyeke@gmail.com>
2020-09-09 00:14:03 +0300

    small fix.

--- models/person.js
+++ models/person.js
@@ -24,6 +24,7 @@ personSchema.set('toJSON', {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
+        return returnedObject
     }
 })
 
